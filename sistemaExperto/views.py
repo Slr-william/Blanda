b5255b4d4b0869129c0fcbd75d4676e770129ac4
William <w.as.g@utp.edu.co>
2016-11-26 00:05:44 -0500

    Mejorado el código para encontrar enfermedad

--- sistemaExperto/views.py
+++ sistemaExperto/views.py
@@ -3,38 +3,38 @@ from django.http.response import HttpResponse
 from .forms import *
 from .models import *
 
-def fname(sintoma):
+def findIll(sintomas):
+    #print "----------------"
+    #print sintomas
+    #print sintomas[0].feature
+    #print "-----------------"
+    illness = []
     a = []
-    sintomas = caracteristica.objects.all()
-    p0 = sintomas.filter(feature = sintoma)
-    if len(p0) >=1:
-        for j in p0[0].objetocaracteristica_set.all():
+    for symptom in sintomas:
+        for j in symptom.objetocaracteristica_set.all():
             a.append(j.obj.name)
-    return a
+        illness.append(a)
+        a = []
+    return illness
 
+def inter(lista):
+    #print "----------------"
+    #print lista
+    #print "-----------------"
+    result = list(set(lista[0]).intersection(*lista))
+    return result
 
 def buscar(request):
+    posibleEnfermedad = []
     form = formBuscar(request.POST or None)
     print(request.POST)
     if request.method == 'POST':
         if form.is_valid():
-            c1 = form.cleaned_data.get('sintoma1')
-            c2 = form.cleaned_data.get('sintoma2')
-            c3 = form.cleaned_data.get('sintoma3')
-            c4 = form.cleaned_data.get('sintoma4')
-
-            a = fname(c1)
-            b = fname(c2)
-            c = fname(c3)
-            d = fname(c4)
-
-            intersection1 = list(set(a) & set(b))
-            intersection2 = list(set(a) & set(b))
-            intersection3 = list(set(intersection1) & set(intersection2))
-
-            if(len(intersection3) == 0):
-                intersection3.append("No se tiene informacion de la enfermedad")
+            a = findIll(form.cleaned_data.values())
+            r = inter(a)
+            if(len(r) == 0):
+                r.append("No se tiene informacion de la enfermedad")
 
 
-            return render(request,'sistemaExperto/index.html',{'form':form, 'enfermedad':intersection3})
+            return render(request,'sistemaExperto/index.html',{'form':form, 'enfermedad':r})
     return render(request,'sistemaExperto/index.html',{'form':form})
