4e16db2e5e991f71c431b10231fcdfd9cc0544ab
William <w.as.g@utp.edu.co>
2016-12-03 20:48:46 -0500

    cambios a sistema experto difuso

--- sistemaExperto/forms.py
+++ sistemaExperto/forms.py
@@ -3,7 +3,7 @@ from .models import *
 
 class formBuscar(forms.Form):
 
-    sintoma1 = forms.ModelChoiceField(required=True, queryset= caracteristica.objects.all().order_by('feature'))
-    sintoma2 = forms.ModelChoiceField(required=True, queryset= caracteristica.objects.all().order_by('feature'))
-    sintoma3 = forms.ModelChoiceField(required=True, queryset= caracteristica.objects.all().order_by('feature'))
-    sintoma4 = forms.ModelChoiceField(required=True, queryset= caracteristica.objects.all().order_by('feature'))
+    sintoma1 = forms.ModelChoiceField(required=False, queryset= caracteristica.objects.all().order_by('feature'))
+    sintoma2 = forms.ModelChoiceField(required=False, queryset= caracteristica.objects.all().order_by('feature'))
+    sintoma3 = forms.ModelChoiceField(required=False, queryset= caracteristica.objects.all().order_by('feature'))
+    sintoma4 = forms.ModelChoiceField(required=False, queryset= caracteristica.objects.all().order_by('feature'))
--- sistemaExperto/views.py
+++ sistemaExperto/views.py
@@ -3,38 +3,64 @@ from django.http.response import HttpResponse
 from .forms import *
 from .models import *
 
+flatten = lambda l: [item for sublist in l for item in sublist]
+
 def findIll(sintomas):
-    #print "----------------"
-    #print sintomas
-    #print sintomas[0].feature
-    #print "-----------------"
     illness = []
     a = []
     for symptom in sintomas:
-        for j in symptom.objetocaracteristica_set.all():
-            a.append(j.obj.name)
-        illness.append(a)
-        a = []
+        if symptom != None:
+            for j in symptom.objetocaracteristica_set.all():
+                a.append(j.obj)
+            illness.append(a)
+            a = []
     return illness
 
-def inter(lista):
-    #print "----------------"
-    #print lista
-    #print "-----------------"
-    result = list(set(lista[0]).intersection(*lista))
-    return result
+def posibles(lista):
+    if len(lista) > 1:
+        aux = lista.pop(0)
+        for i in lista:
+            aux = set(aux) | set(i)
+        print aux
+    else:
+        return lista[0]
+    return list(aux)
+
+def fuzzy(illness, symptoms ):
+    t = 0
+    points = len(illness)*[0.0]
+    nIll = 0
+    for i in illness:
+        for j in i.objetocaracteristica_set.all():
+            for s in symptoms:
+                if s == j.fea:
+                    points[nIll]+=1.0
+                    t+=1.0
+        nIll+=1
+        print points
+
+    for i in range(len(points)):
+        div = points[i] / t
+        div = 100 if div > 1 else div*100
+        points[i] = [div,illness[i]]
+
+    return points
 
 def buscar(request):
     posibleEnfermedad = []
     form = formBuscar(request.POST or None)
-    print(request.POST)
+    #print(request.POST)
     if request.method == 'POST':
         if form.is_valid():
-            a = findIll(form.cleaned_data.values())
-            r = inter(a)
-            if(len(r) == 0):
-                r.append("No se tiene informacion de la enfermedad")
+            symptoms = form.cleaned_data.values()
+            a = findIll(symptoms)
+            illness = posibles(a)
+
+            if(len(illness) == 0):
+                result = ["No se tiene informacion de la enfermedad"]
+            else:
+                result = fuzzy(illness, symptoms)
 
 
-            return render(request,'sistemaExperto/index.html',{'form':form, 'enfermedad':r})
+            return render(request,'sistemaExperto/index.html',{'form':form, 'enfermedad':result})
     return render(request,'sistemaExperto/index.html',{'form':form})
